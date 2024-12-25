fe581b5017240e3e5a8b0d1319f2df60389ae345
Joel Wetzell <jwetzell@yahoo.com>
2024-12-25 09:31:22 -0600

    cleanup logging

--- examples/udp_client/main.go
+++ examples/udp_client/main.go
@@ -10,13 +10,13 @@ import (
 func main() {
 	addr, err := net.ResolveUDPAddr("udp", "0.0.0.0:3333")
 	if err != nil {
-		slog.Error("error decoding", "err", err)
+		slog.Error("error making UDP address", "err", err)
 		return
 	}
 
 	client, err := net.ListenUDP("udp", addr)
 	if err != nil {
-		slog.Error("error decoding", "err", err)
+		slog.Error("error listening to UDP", "err", err)
 		return
 	}
 	defer client.Close()
@@ -26,7 +26,7 @@ func main() {
 
 		length, _, err := client.ReadFromUDP(buffer)
 		if err != nil {
-			slog.Error("error decoding", "err", err)
+			slog.Error("error reading from UDP", "err", err)
 		} else if length > 0 {
 			message, err := freeD.Decode(buffer[0:length])
 			if err != nil {
